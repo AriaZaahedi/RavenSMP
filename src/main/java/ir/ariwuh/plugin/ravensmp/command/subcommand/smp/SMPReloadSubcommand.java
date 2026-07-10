cd87f1e54d7aa82c438d1e2b7f02147ef25b2adc
Aria Zaahedi <AriaZaahedi@users.noreply.github.com>
2026-07-10 13:13:16 +0330

    feat: Reload languages via /smp reload.

--- src/main/java/ir/ariwuh/plugin/ravensmp/command/subcommand/smp/SMPReloadSubcommand.java
+++ src/main/java/ir/ariwuh/plugin/ravensmp/command/subcommand/smp/SMPReloadSubcommand.java
@@ -2,6 +2,7 @@ package ir.ariwuh.plugin.ravensmp.command.subcommand.smp;
 
 import ir.ariwuh.plugin.ravensmp.command.api.SubCommand;
 import ir.ariwuh.plugin.ravensmp.command.api.SubCommandHandler;
+import ir.ariwuh.plugin.ravensmp.manager.LanguageManager;
 import ir.ariwuh.plugin.ravensmp.manager.PluginSettingsManager;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
@@ -9,15 +10,17 @@ import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.NonNull;
 
 @RequiredArgsConstructor
-@SubCommand(label = "reload", description = "Reload plugin settings.")
+@SubCommand(label = "reload", description = "Reload plugin config.")
 public final class SMPReloadSubcommand extends SubCommandHandler {
 
     private final @NotNull PluginSettingsManager pluginSettingsManager;
+    private final @NotNull LanguageManager languageManager;
 
     @Override
     public void execute(@NotNull Player player, @NonNull String[] arguments) {
         this.pluginSettingsManager.reloadPluginSettings();
-        player.sendRichMessage("<green>Plugin settings have been reloaded successfully.");
+        this.languageManager.reloadLanguages();
+        player.sendRichMessage("<green>Plugin settings and languages have been reloaded successfully.");
     }
 
 }
